968f90122acaa01ec24bb1fd792e6adbe8134d80
dayCod <alayarahman07@gmail.com>
2023-09-23 13:07:49 +0700

    Add: Simple Livewire Comment

---
+++ resources/views/livewire/comments.blade.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-12">
+            <h1 class="fw-bold">Comments</h1>
+        </div>
+        <div class="col-12 mt-5">
+            <div class="input-group mb-3">
+                <input type="text" placeholder="What's in your mind" class="form-control" aria-describedby="button-addon2" wire:model="newComment">
+                <button class="btn btn-primary" type="button" id="button-addon2" wire:click="addComment">Add</button>
+            </div>
+        </div>
+        <div class="col-12">
+            @foreach($comments as $comment)
+            <div class="card my-3">
+                <div class="card-body">
+                    <div class="d-flex gap-3">
+                        <h5 class="card-title">{{ $comment['creator'] }}</h5>
+                        <span class="text-muted" style="font-size: 10pt">{{ $comment['created_at'] }}</span>
+                    </div>
+                    <p class="card-text">{{ $comment['body'] }}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
